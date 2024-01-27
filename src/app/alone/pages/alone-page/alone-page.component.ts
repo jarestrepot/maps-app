0efce2fa32c021e68eda34fe6e3285dd2104ff11
Javier  Restrepo Toro <jarestrepot@uqvirtual.edu.co>
2024-01-27 17:14:27 +0100

    StandAlone component in page

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
+
+@Component({
+  standalone: true,
+  imports: [
+    CommonModule,
+    CounterAloneComponent
+  ],
+  templateUrl: './alone-page.component.html',
+  styleUrls: ['./alone-page.component.scss']
+})
+export class AlonePageComponent {
+
+}
